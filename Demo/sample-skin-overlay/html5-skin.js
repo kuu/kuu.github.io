f35bf867689fd348832e4392743edddc671886f6
Kuu Miyazaki <miyazaqui@gmail.com>
2016-08-03 22:19:29 +0900

    Update Demo/sample-skin-overlay

--- Demo/sample-skin-overlay/html5-skin.js
+++ Demo/sample-skin-overlay/html5-skin.js
@@ -6566,7 +6566,7 @@ var PlayingScreen = React.createClass({displayName: "PlayingScreen",
 
       this.props.controller.state.buffering ? React.createElement(Spinner, {loadingImage: this.props.skinConfig.general.loadingImage.imageResource.url}) : null, 
 
-      !this.isMobile && this.props.fullscreen ? React.createElement("div", {className: "oo-playing-overlay-large"}) : React.createElement("div", {className: "oo-playing-overlay-normal"}), 
+      "//", !this.isMobile && this.props.fullscreen ? React.createElement("div", {className: "oo-playing-overlay-large"}) : React.createElement("div", {className: "oo-playing-overlay-normal"}), 
 
       React.createElement("div", {className: "oo-interactive-container"}, 
 
